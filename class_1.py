d23d65ec492deb6d854120805a4befbd74489459
shazedul-jewel <jewel8102631@gmail.com>
2024-12-14 12:35:43 +0600

    new variable

---
+++ class_1.py
@@ -0,0 +1,82 @@
+#print("Hello World")
+#x = 5
+#y = x
+#print(x)
+#print(y)
+
+#x = 4 # x is of type int
+#x = "Sally" # x is now of type str
+#print(x)
+#print(x)
+
+#x, y, z = "Orange", "Banana", "Cherry"
+#print(z)
+#print(x)
+#print(y)
+
+
+#username = input("Enter username:")
+#print("Username is: " + username)
+
+
+#x = 5
+#x += 3
+#print(x)
+
+#x = y = z = "Orange"
+#print(x)
+#print(y)
+#print(z)
+
+#x = 1 # int
+#y = 2.8 # float
+#z = 1j # complex
+#
+#print(type(x))
+#print(type(y))
+
+#u=input()
+#m=input()
+#print (u,m)
+
+#i = 1
+#while i < 101:
+#  print(i)
+#  i += 1
+
+#fruits = ['apple', 'banana', 'cherry']
+#
+#x = fruits.index("banana")
+#
+#print(x)
+
+#x = "Python"
+#y = "is"
+#z = "awesome"
+#print(x, y, z)
+
+#x = "Python "
+#y = "is "
+#z = "awesome"
+#print(x + y + z)
+
+
+#username = input("Enter username:")
+#lastame = input("Enter username:")
+#print(username , lastame)
+
+
+#x = 5
+#x+=3
+#print(x)
+
+#x =10
+#z =not (x > 11 or x < 9)
+#print (z)
+
+
+
+ 
+
+
+
